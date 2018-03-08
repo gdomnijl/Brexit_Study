391051947c8dbfe618d483ce0410c198fffd7bbd
gdomnijl <gdomnijl@gmail.com>
2018-03-08 09:28:45 -0600

    to merge

--- Building_dataset_for_model.r
+++ Building_dataset_for_model.r
@@ -1,4 +1,7 @@
+## Building dataset with opinion switch columns
+
 library(tidyverse)
+
 ## getting the columns: id, wave, euRefVote
 dat <-read.csv("/Users/apple/Desktop/DAT/BriExit/data/wt_age_origin.csv")
 col_wave <- grep("^wave\\d+",names(dat))
@@ -6,7 +9,6 @@ col_vote <- grep("^euRefVoteW\\d+",names(dat))
 
 opinion_dat <- dat %>% 
   select(c(id,col_wave,col_vote))
-  
 
 # 6301 cases who DID participated in the study but has an NA response
 temp <- c() ## all responses
@@ -41,6 +43,8 @@ for(i in 1:13){
 
 temp <- temp %>% arrange(id)
 
+# Citation:
+# http://www.cookbook-r.com/Manipulating_data/Filling_in_NAs_with_last_non-NA_value/
 fillNAgaps <- function(x, firstBack=FALSE) {
   ## NA's in a vector or factor are replaced with last non-NA values
   ## If firstBack is TRUE, it will fill in leading NA's with the first
@@ -73,6 +77,7 @@ fillNAgaps <- function(x, firstBack=FALSE) {
   x
 }
 
+## Extend NA values
 temp_full <- temp %>%
   group_by(id) %>%
   mutate(full_vote = fillNAgaps(vote)) %>%
@@ -80,38 +85,12 @@ temp_full <- temp %>%
   mutate(switch = ifelse(wave==1, 0, 
                          ifelse(full_vote!=pre_vote, 1, 0))) 
 
+## Compute switch
 switch_dat <- temp_full %>%
   summarise(num_wave_present = sum(present),
             num_switch = sum(switch,na.rm = TRUE)) %>% 
   mutate(switch_ratio = num_switch/num_wave_present,
          ifswitch = ifelse(num_switch>0, 1,0))
 
-write.csv(switch_dat, "opinion_switch.csv")
-
-sapply(opinion_dat,count_switch)
-num_switch <- c()
-
-count_switch <- function(row){
-  index <- grep("^euRefVoteW\\d+", names(opinion_dat))
-  first <- index[1]
-  pre_opinion <- row[first]
-  
-  switch_count <- 0
-  in_count <- 0
-  
-  for(i in index){
-    print(i)
-    cur_opinion <- row[i]
-    if(!is.na(cur_opinion)){
-      
-      # there is a switch
-     if(cur_opinion != pre_opinion){
-      switch_count <- switch_count + 1
-      pre_opinion <- cur_opinion
-     }
-      in_count <- in_count + 1
-    }
-    print(paste0("switch:", switch_count, "  in:", in_count))
-  }
-  return(switch_count)
-}
+dataset_static <- inner_join(switch_dat,static)
+write.csv(dataset_static, "data/dataset_static_factors.csv")
