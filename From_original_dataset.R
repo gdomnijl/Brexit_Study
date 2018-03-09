d064b73944b673b80fec1f26b143c7271f07705e
gdomnijl <gdomnijl@gmail.com>
2018-03-08 22:11:11 -0600

    lasso modeling

--- From_original_dataset.R
+++ From_original_dataset.R
@@ -2,8 +2,6 @@ library(readstata13)
 library(tidyverse)
 dataset <- read.dta("H:/DAT/BrExit/data/BES2017_W13_Panel_v1.2.dta")
 
-
-
 de <- c("competent", "authLong", "grammar", "LookAfter", "overseasAid","^W1","^W2","^W3","^W4","^W5","^W6","^W10","^W11","^W12","^W13")
 for(i in 1:length(remove)){
   index <-grep(remove[i], names(dataset))
@@ -11,11 +9,8 @@ for(i in 1:length(remove)){
     select(-index)
 }
 
-
 opinion_index <- grep("euRefVoteW",colnames(dataset))
 opinion <- dataset()
-
-
 
 ## filter out variable names with 'Wi' or 'wi'
 wave_spec_index <- c(grep("W\\d+", colnames(dataset)), grep("w\\d+",colnames(dataset)))
