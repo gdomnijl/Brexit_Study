8728380a081510ca13d1472d88affd6dba874305
He <hejinlin@grinnell.edu>
2018-04-17 15:35:06 -0500

    standardize response for each immigration question

--- modeling/feature_engineering.r
+++ modeling/feature_engineering.r
@@ -15,13 +15,42 @@ for(i in 2:length(names(immig_data))){
 #immigrationLevel # 1: decreased a lot, 5 increased a lot, 9: dk
 #changeImmig # 1:a lot lower, 5: a lot higher, 6: dk
 #immigEcon # 1:bad 7:good 8:dk
-#immigCultureal # 1:bad 7:good 8:dk
+#immigCultural # 1:bad 7:good 8:dk
+#immigrantsWelfareState # 1: strongly disagree 5: strongly agree 6:dk
+
 #controlImmig # 1:no control 5:complete control 6:dk
 #effectsEUImmigration # 1: much lower 5:much higher 6: dk
-#immigrantsWelfareState # 1: strongly disagree 5: strongly agree 6:dk
 
 write.csv(immig_data, "data/immig_cols.csv")
 
 
 ## standardize all responses between 0 and 1
-## 0: anti-immigrants 1: pro-immigrants
+## 1: anti-immigrants 0: pro-immigrants
+lvl <- grep("immigrationLevel", names(immig_data))
+chg <- grep("changeImmig", names(immig_data))
+econ <- grep("immigEcon", names(immig_data))
+cul <- grep("immigCultural", names(immig_data))
+wel <- grep("immigrantsWelfareState", names(immig_data))
+ctr <- grep("controlImmig", names(immig_data))
+eu <- grep("effectsEUImmigration", names(immig_data))
+
+for(i in 2:length(names(immig_data))){
+  ## TODO: change dk to NA
+  
+  ## 5 is pro
+  if(i %in% c(lvl, eu)){
+    immig_data[,i] <- (immig_data[,i]-1)/4
+  } 
+  
+  ## 1 is pro
+  if(i %in% c(ctr,wel,chg)){
+    immig_data[,i] <- 1-(immig_data[,i]-1)/4
+  }
+  
+  ## 7 is pro
+  if(i %in% c(econ, cul)){
+    immig_data[,i] <- (immig_data[,i]-1)/6
+  } 
+  
+}
+
